67a5a775d850aab913a921fe2f15873b1bd91e73
liu.jun <sovranliu@aliyun.com>
2016-03-07 20:10:46 +0800

    ...

--- src/com/qcast/tower/view/form/SelfDiagnosticActivity.java
+++ src/com/qcast/tower/view/form/SelfDiagnosticActivity.java
@@ -8,11 +8,16 @@ import java.util.HashMap;
 import com.qcast.tower.R;
 import com.slfuture.pluto.communication.Host;
 import com.slfuture.pluto.communication.response.CommonResponse;
+import com.slfuture.pluto.communication.response.JSONResponse;
 import com.slfuture.pluto.communication.response.Response;
+import com.slfuture.pluto.etc.GraphicsHelper;
+import com.slfuture.pluto.view.annotation.ResourceView;
+import com.slfuture.pluto.view.component.ActivityEx;
 import com.slfuture.carrie.base.json.JSONArray;
 import com.slfuture.carrie.base.json.JSONNumber;
 import com.slfuture.carrie.base.json.JSONObject;
 import com.slfuture.carrie.base.json.JSONString;
+import com.slfuture.carrie.base.json.JSONVisitor;
 import com.slfuture.carrie.base.json.core.IJSON;
 
 import android.app.Activity;
@@ -24,6 +29,7 @@ import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ListView;
@@ -36,7 +42,22 @@ import android.widget.SimpleAdapter.ViewBinder;
 /**
  * 自我诊断页
  */
-public class SelfDiagnosticActivity extends Activity {
+@ResourceView(id = R.layout.activity_selfdiagnostic)
+public class SelfDiagnosticActivity extends ActivityEx {
+	@ResourceView(id = R.id.selfdiagnostic_button_graph)
+	public Button btnGraph;
+	@ResourceView(id = R.id.selfdiagnostic_button_list)
+	public Button btnList;
+	@ResourceView(id = R.id.selfdiagnostic_layout_graph)
+	public View viewGraph;
+	@ResourceView(id = R.id.selfdiagnostic_layout_list)
+	public View viewList;
+	@ResourceView(id = R.id.selfdiagnostic_list_body)
+	public ListView listBody;
+	@ResourceView(id = R.id.selfdiagnostic_list_symptom)
+	public ListView listSymptom;
+
+
 	/**
 	 * 当前部位列表
 	 */
@@ -49,23 +70,18 @@ public class SelfDiagnosticActivity extends Activity {
 	 * 当前肢体ID
 	 */
 	private int currentBody = 0;
-	
-	
+
+
 	/**
 	 * 界面创建
 	 */
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		Log.i("TOWER", "SelfDiagnosticActivity.onCreate() execute");
 		super.onCreate(savedInstanceState);
-		requestWindowFeature(Window.FEATURE_NO_TITLE);
-		setContentView(R.layout.activity_selfdiagnostic);
-		// 界面处理
 		prepare();
-		//
 		loadBody();
 	}
-	
+
 	/**
 	 * 过滤器回调
 	 */
@@ -80,7 +96,49 @@ public class SelfDiagnosticActivity extends Activity {
 			selectBody(getBodyId(data.getIntExtra("body", 0)));
 		}
 	}
-	
+
+	/**
+	 * 界面预处理
+	 */
+	public void prepare() {
+		btnGraph.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				viewGraph.setVisibility(View.VISIBLE);
+				viewList.setVisibility(View.GONE);
+			}
+		});
+		btnList.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				viewGraph.setVisibility(View.GONE);
+				viewList.setVisibility(View.VISIBLE);
+			}
+		});
+		
+		
+		
+		currentBody = getBodyId(this.getIntent().getIntExtra("body", 0));
+		ImageButton button = (ImageButton) this.findViewById(R.id.selfdiagnostic_button_return);
+		button.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				SelfDiagnosticActivity.this.finish();
+			}
+		});
+		TextView text = (TextView) this.findViewById(R.id.selfdiagnostic_text_filter);
+		text.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(SelfDiagnosticActivity.this, BodyActivity.class);
+				SelfDiagnosticActivity.this.startActivity(intent);
+				SelfDiagnosticActivity.this.finish();
+			}
+		});
+		dealBody();
+		dealSymptom();
+	}
+
 	public int getBodyId(int imgId) {
 		switch(imgId) {
 		case R.id.male_front_head:
@@ -121,31 +179,6 @@ public class SelfDiagnosticActivity extends Activity {
 			return(12);
 		}
 		return 0;
-	}
-	
-	/**
-	 * 界面预处理
-	 */
-	public void prepare() {
-		currentBody = getBodyId(this.getIntent().getIntExtra("body", 0));
-		ImageButton button = (ImageButton) this.findViewById(R.id.selfdiagnostic_button_return);
-		button.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				SelfDiagnosticActivity.this.finish();
-			}
-		});
-		TextView text = (TextView) this.findViewById(R.id.selfdiagnostic_text_filter);
-		text.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				Intent intent = new Intent(SelfDiagnosticActivity.this, BodyActivity.class);
-				SelfDiagnosticActivity.this.startActivity(intent);
-				SelfDiagnosticActivity.this.finish();
-			}
-		});
-		dealBody();
-		dealSymptom();
 	}
 	
 	/**
@@ -230,39 +263,24 @@ public class SelfDiagnosticActivity extends Activity {
 	}
 	
 	/**
-	 * 加载部位列表
+	 * 加载器官列表
 	 */
 	public void loadBody() {
-		Host.doCommand("loadBody", new CommonResponse<String>() {
-			@Override
-			public void onFinished(String content) {
-				if(Response.CODE_SUCCESS != code()) {
-					Toast.makeText(SelfDiagnosticActivity.this, "加载身体部位列表失败", Toast.LENGTH_LONG).show();
-					return;
-				}
-				JSONObject resultObject = JSONObject.convert(content);
-				if(((JSONNumber) resultObject.get("code")).intValue() <= 0) {
-					Toast.makeText(SelfDiagnosticActivity.this, ((JSONString) resultObject.get("msg")).getValue(), Toast.LENGTH_LONG).show();
-					return;
-				}
-				JSONArray result = (JSONArray) resultObject.get("data");
-				bodyList.clear();
-				for(IJSON item : result) {
-					JSONString newJSONString = (JSONString) item;
-					String caption = newJSONString.getValue();
-					HashMap<String, Object> bodyMap = new HashMap<String, Object>();
-					bodyMap.put("caption", caption);
-					bodyList.add(bodyMap);
-				}
-				ListView listview = (ListView) SelfDiagnosticActivity.this.findViewById(R.id.selfdiagnostic_list_body);
-				SimpleAdapter adapter = (SimpleAdapter) listview.getAdapter();
-				adapter.notifyDataSetChanged();
-				//
-				selectBody(currentBody);
-			}
-		});
+		String text = Host.fetchMock("loadBody");
+		if(null == text) {
+			return;
+		}
+		bodyList.clear();
+		for(JSONVisitor item : (new JSONVisitor(JSONArray.convert(text))).toVisitors()) {
+			HashMap<String, Object> bodyMap = new HashMap<String, Object>();
+			bodyMap.put("name", item.getString("name"));
+			bodyMap.put("image", GraphicsHelper.decodeResource(context, resourceId) item.getString("image"));
+			bodyMap.put("image_selected", item.getString("image_selected"));
+			bodyList.add(bodyMap);
+		}
+		((SimpleAdapter) listBody.getAdapter()).notifyDataSetChanged();
 	}
-	
+
 	/**
 	 * 加载症状列表
 	 * 
